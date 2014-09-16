2482186f46dc25eed703f1632255156c720ee8d5
Martin Vach <mv@zwaveeurope.com>
2014-09-16 12:24:34 +0200

    IP FIX

--- app/core/controllers/controllers.js
+++ app/core/controllers/controllers.js
@@ -76,7 +76,8 @@ appController.controller('BaseController', function($scope, $cookies, $filter, $
     $scope.setIP = function(ip) {
        $http.get(ip).success(function(data, status, headers, config) {
            $scope.customIP.message = false;
-            cfg.server_url = ip;
+            //cfg.server_url = ip;
+            cfg.server_url = 'http://' + ip + ':8083';
             $route.reload();
         }).error(function(data, status, headers, config) {
             $scope.customIP.message = $scope._t('error_handling_data') + ': ' + ip;
