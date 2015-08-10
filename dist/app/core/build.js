4b5068a0a4714f74e264c2af70b2683669453b70
Martin Vach <mv@zwaveeurope.com>
2015-08-10 13:51:34 +0200

    DIST version

--- dist/app/core/build.js
+++ dist/app/core/build.js
@@ -9619,6 +9619,32 @@ angApp.filter('stringToSlug', function() {
         return str;
     };
 });
+
+/**
+ * Cut text into x chars
+ */
+angApp.filter('cutText', function() {
+    return function(value, wordwise, max, tail) {
+        if (!value)
+            return '';
+
+        max = parseInt(max, 10);
+        if (!max)
+            return value;
+        if (value.length <= max)
+            return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' …');
+    };
+});
 /**
  * Get object length
  */
