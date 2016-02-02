fb01d0d132b52b0d442917ffd3d9dded92238165
Martin Vach <mv@zwaveeurope.com>
2016-02-02 11:35:32 +0100

    v1.0.4-RC1

--- app/core/config.js
+++ app/core/config.js
@@ -6,7 +6,7 @@
 var config_data = {
     'cfg': {
         'app_name': 'Z-Wave',
-        'app_version': '1.0.3 ',
+        'app_version': '1.0.4 ',
         'custom_ip': false,
         'user_field': 'USERXXXX',
         'pass_field': 'PSWDXXXX',
