7ddba6a0c53d3fcd0a8044bc2aa67380b5ef4022
Martin Vach <mv@zwaveeurope.com>
2016-11-23 09:53:49 +0100

    Configuration/Firmware update refactoring

--- app/controllers/configuration_firmware.js
+++ app/controllers/configuration_firmware.js
@@ -8,7 +8,7 @@
  * @class ConfigFirmwareController
  *
  */
-appController.controller('ConfigFirmwareController', function ($scope, $routeParams, $location, $cookies, cfg, dataService, deviceService) {
+appController.controller('ConfigFirmwareController', function ($scope, $routeParams, $location, $cookies, $timeout,cfg, dataService, deviceService) {
     $scope.devices = [];
     $scope.deviceId = 0;
     $scope.activeTab = 'firmware';
@@ -52,67 +52,31 @@ appController.controller('ConfigFirmwareController', function ($scope, $routePar
             $location.path($scope.activeUrl + deviceId);
         }
     };
-
-    $scope.updateDeviceFirmware = function (input) {
-        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('initiating_device_firmware_update')};
+    /**
+     * Handles device firmware update
+     * @param {object} input
+     * @param {string} id
+     */
+    $scope.updateDeviceFirmware = function (input,id) {
+        $scope.toggleRowSpinner(id);
         var cmd = cfg.server_url + cfg.fw_update_url + '/' + $scope.deviceId
         var fd = new FormData();
 
         fd.append('file',$scope.myFile);
         fd.append('url', input.url);
-        fd.append('targetId', input.targetId);
+        fd.append('targetId', input.targetId|| '0');
         dataService.uploadApiFile(cmd, fd).then(function (response) {
-            $scope.loading = false;
+            $timeout($scope.toggleRowSpinner, 1000);
             deviceService.showNotifier({message: $scope._t('success_device_firmware_update')});
             /*$timeout(function () {
                 alertify.dismissAll();
                 $window.location.reload();
             }, 2000);*/
         }, function (error) {
-            $scope.loading = false;
+            $timeout($scope.toggleRowSpinner, 1000);
             alertify.alertError($scope._t('error_device_firmware_update'));
         });
     };
-    $scope.load($routeParams.nodeId);
-    /**
-     * update Firmware
-     * todo: complete this function
-     */
-    $scope.updateFirmware = function (nodeId) {
-        if (!$scope.formFirmware.url && !$scope.formFirmware.targetId) {
-            return;
-        }
-        // $('.fa-spin').show();
 
-        // File upload test
-        var data = {
-            'url': $scope.formFirmware.url,
-            'file': $scope.myFile,
-            'targetId': $scope.formFirmware.targetId
-        };
-
-//        dataService.joinedZwaveData(function(data) {
-//            $scope.firmwareProgress++;
-//            console.log($filter('hasNode')(data.update,'FirmwareUpdate.data.fragmentTransmitted.value'));
-//
-////                refresh(data.update);
-//        });
-
-        // Watch for progress change
-        $scope.$watch('firmwareProgress', function () {
-            if ($scope.firmwareProgress >= 100) {
-                $('.fa-spin').fadeOut();
-                dataService.cancelZwaveDataInterval();
-            }
-
-        });
-        // Cancel interval on page destroy
-        $scope.$on('$destroy', function () {
-            dataService.cancelZwaveDataInterval();
-        });
-
-        dataService.fwUpdate(nodeId, data);
-        return;
-    };
 
 });
