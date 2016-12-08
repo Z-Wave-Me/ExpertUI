8e5a22bdaa3e639115728ac8e3453f260f31c866
Michael Hensche <mh@zwave.eu>
2016-12-08 14:25:13 +0100

    fix bug newline homeNotes

--- app/controllers/expert_config.js
+++ app/controllers/expert_config.js
@@ -59,7 +59,10 @@ appController.controller('DataHolderController', function ($scope, $timeout,$int
      */
     $scope.storeNotes = function(input,spin) {
         $scope.toggleRowSpinner(spin);
-        dataService.postApi('store_url', null, 'controller.data.homeNotes.value="'+input.replace(/\"/g, '\'')+'"').then(function (response) {
+
+        input = input.replace(/\"/g, '\'');
+        input = input.replace(/\n/g, '<br>');
+        dataService.postApi('store_url', null, 'controller.data.homeNotes.value="'+input+'"').then(function (response) {
             $scope.save();
         }, function (error) {
             $scope.toggleRowSpinner();
