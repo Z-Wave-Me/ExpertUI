04e308e121f1033a568e7786062a226bf8bc1e73
Martin Vach <mv@zwaveeurope.com>
2017-06-30 10:46:43 +0200

    Release Z-Wave CIT 1.3.0

--- cit/app/js/config.js
+++ cit/app/js/config.js
@@ -9,7 +9,7 @@ var config_data = {
         'dev_host': [],
         'app_name': 'Z-Wave CIT',
         'app_version': '1.3.0',
-        'app_built': '29-06-2017 14:41:01',
+        'app_built': '30-06-2017 10:46:01',
         'app_id': 'ExpertUI',
         'custom_ip': false,
         'user_field': 'USERXXXX',
