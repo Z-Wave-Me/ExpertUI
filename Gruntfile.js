ea490134b9e08a3a0e42204cc8acb110220e73b8
Poltorak Serguei <ps@z-wave.me>
2017-08-11 03:32:14 +0300

    RouteMap files updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,8 @@ module.exports = function (grunt) {
                     'vendor/justgagejs/justgage.js',
                     'vendor/upload/angular-file-upload-shim.min.js',
                     'vendor/alertify/alertify.min.js',
+                    'vendor/routemap/ZWaveAnalytics.min.js',
+                    'vendor/routemap/ZWaveRouteMapDraw.min.js',
                     // Angular
                     'vendor/angular/angular-1.2.14/angular.min.js',
                     'vendor/upload/angular-file-upload.min.js',
