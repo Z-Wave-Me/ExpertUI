c219a8b02518877d6efa8c1f99f42c76e502a2e0
Martin Vach <mv@zwaveeurope.com>
2018-02-15 13:56:05 +0100

    CIT-265 Allow to upload files with lower/upper case extension

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -911,29 +911,7 @@ angApp.filter('fileExtension', function () {
         }
 
         // extract extension ignoring `.`
-        return basename.slice(pos + 1);
-    };
-});
-
-/**
- * Get a file extension from the path
- * @function fileExtension
- */
-angApp.filter('fileExtension', function () {
-    return function (path) {
-        // extract file name from full path ...
-        // (supports `\\` and `/` separators)
-        var basename = path.split(/[\\/]/).pop(),
-            // get last position of `.`
-            pos = basename.lastIndexOf(".");
-        // if file name is empty or ...
-        //  `.` not found (-1) or comes first (0)
-        if (basename === '' || pos < 1) {
-            return '';
-        }
-
-        // extract extension ignoring `.`
-        return basename.slice(pos + 1);
+        return basename.slice(pos + 1).toLowerCase();
     };
 });
 
