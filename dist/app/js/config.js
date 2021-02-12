07841a9e50a3bc7aa871e930b6a72778176a26d2
Poltorak Serguei <ps@z-wave.me>
2021-02-12 04:37:49 +0300

    dist

--- dist/app/js/config.js
+++ dist/app/js/config.js
@@ -9,7 +9,7 @@ var config_data = {
 		'dev_host': [],
 		'app_name': 'Z-Wave Expert',
 		'app_version': 'v1.5.4',
-		'app_built': '05-02-2021 02:28:00',
+		'app_built': '12-02-2021 04:37:49',
 		'app_id': 'ExpertUI',
 		'custom_ip': false,
 		'user_field': 'USERXXXX',
