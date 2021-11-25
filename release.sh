9d947c4f01999c4d0315d63fdc05ef3811fea783
Poltorak Serguei <ps@z-wave.me>
2021-11-26 01:31:52 +0300

    release.sh

--- release.sh
+++ release.sh
@@ -33,9 +33,9 @@ else
 	echo "Building test version"
 fi
 
-(cd app/sass/; ./compile.sh)
+(cd app/sass/; ./compile.sh) || exit
 
-grunt
+grunt || exit
 
 if [ "$1" == "dist" ]; then
 	git add app/css/main.css app/css/main.css.map app/info.json package.json dist/ &&
